b9f0e4b878c65d704db448e488fb5ea24c70d60d
charlotte pinoit <charlotte.pinoit@le-campus-numerique.fr>
2026-03-02 14:07:46 +0100

    remove .idea .iml

--- src/Character.java
+++ src/Character.java
@@ -2,6 +2,16 @@ public class Character{
     //attributs privés
     private String type;
     private String name;
-    private int ttack
+    private int life;
+    private int attack;
+
+
+    //constructeur
+    public Character(String type, int life, int attack, String name){
+        this.type = type;
+        this.life = life;
+        this.attack = atta
+
+    }
 
 }
